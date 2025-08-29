68a7fc3dd2a13ebebb1f7133ebe9e90d330b0d13
Shivam Mittal <shivam.103mittal@gmail.com>
2025-08-30 01:56:04 +0530

    Downgrade tailwind to prevent breaking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'ui-sans-serif', 'system-ui']
-      },
-      colors: {
-        primary: '#4F46E5', // Indigo
-        secondary: '#06B6D4', // Cyan
-        accent: '#F59E0B' // Amber
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
